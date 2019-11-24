553882a894af475232c40975afa0d5eb5e748cf3
shariful islam <sisagor98@gmail.com>
2019-11-24 10:10:54 +0600

    mechanism AngryBird

---
+++ Assets/Script/BallCntoller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BallCntoller : MonoBehaviour
+{
+    public Rigidbody2D hook;
+    public float maxDragDistance = 2f;
+
+    public float releageTime = .15f;
+    public Rigidbody2D rigidbody;
+    private bool isPressed = false;
+
+
+   public GameObject Nextboll;
+
+    private void Update() {
+
+        if(isPressed){
+            Vector2 mousePos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePos,hook.position) > maxDragDistance){
+                rigidbody.position = hook.position + (mousePos -hook.position).normalized * maxDragDistance;
+            }
+            else{
+                 rigidbody.position  = mousePos;
+            }
+        }
+           
+    }
+
+    void OnMouseDown() {
+        isPressed =true;
+        rigidbody.isKinematic = true;
+    }
+    private void OnMouseUp() {
+
+        isPressed = false;
+        rigidbody.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+
+    IEnumerator Release(){
+        yield return new WaitForSeconds(releageTime);
+        GetComponent<SpringJoint2D>().enabled = false ;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if(Nextboll != null){
+            Nextboll.SetActive(true);
+        }
+        else{
+            Enemy.EnemyCount = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+}
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SecneManager;
+
+public class Enemy : MonoBehaviour
+{
+    public float health= 4f;
+
+     public static int EnemyCount= 0;
+
+    private void Start() {
+        EnemyCount++;
+    }
+     private void OnCollisionEnter2D(Collision2D other) {
+
+         //Debug.Log(other.relativeVelocity.magnitude);
+
+          if(other.relativeVelocity.magnitude > health ){
+              Die();
+          }
+            
+     }
+
+     void Die(){
+         EnemyCount--;
+         if(EnemyCount<=0)
+         {
+             Debug.Log("Level win");
+         }
+         Destroy(this.gameObject);
+     }
+
+}
